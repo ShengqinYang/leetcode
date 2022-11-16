f43faf3482433cecc393f7606d942168a78c382e
yangshengqin <18513100108@163.com>
2022-11-16 16:25:15 +0800

    Leetcode-26-删除有序数组的重复值.py

---
+++ python_algo/Leetcode-26-删除有序数组的重复值.py
@@ -0,0 +1,19 @@
+# 26. 删除有序数组中的重复项
+class Solution:
+    def removeDuplicates(self, nums):
+        length = len(nums)
+        p, q = 0, 1
+        while q < length:
+            if nums[p] != nums[q]:
+                if q - p > 1:
+                    nums[p + 1] = nums[q]
+                p += 1
+            q += 1
+        return p + 1, nums[:p + 1]
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 2, 2, 3, 3, 4, 5, 6, 6, 6, 6, 7, 9]
+    sol = Solution()
+    index, result = sol.removeDuplicates(nums)
+    print(index, result)
